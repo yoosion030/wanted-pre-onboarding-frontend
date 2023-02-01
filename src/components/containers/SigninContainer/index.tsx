e5e9552d1575489976770598a484f697eddbf769
yoosion030 <rsy5487@naver.com>
2023-02-01 21:17:24 +0900

    :sparkles: 로그인 API 연동 및 토큰 처리

--- src/components/containers/SigninContainer/index.tsx
+++ src/components/containers/SigninContainer/index.tsx
@@ -1,18 +1,63 @@
-import * as S from "./style";
+import { Input, Button, AuthForm } from "components";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import instance from "shared/instance";
 
 const SigninContainer = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [emailError, setEmailError] = useState<boolean>(true);
+  const [passwordError, setPasswordError] = useState<boolean>(true);
+
+  const handleSignin = async (e: any) => {
+    e.preventDefault();
+    try {
+      const { data } = await instance.post("/auth/signin", { email, password });
+      localStorage.setItem("access_token", data.access_token);
+      navigate("/todo");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (email.includes("@")) setEmailError(false);
+  }, [email]);
+
+  useEffect(() => {
+    if (password.length >= 8) setPasswordError(false);
+    else setPasswordError(true);
+  }, [password]);
+
   return (
-    <S.Conainer>
-      <S.SigninForm>
-        <div>
-          <S.Label>이메일</S.Label>
-          <S.EmailInput data-testid="email-input" />
-          <S.Label>비밀번호</S.Label>
-          <S.PasswordInput data-testid="password-input" />
-        </div>
-        <S.SubmitButton data-testid="signup-button">로그인</S.SubmitButton>
-      </S.SigninForm>
-    </S.Conainer>
+    <AuthForm>
+      <div>
+        <p>이메일</p>
+        <Input
+          data-testid="email-input"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
+        <p>비밀번호</p>
+        <Input
+          data-testid="password-input"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+          type="password"
+        />
+      </div>
+      <Button
+        data-testid="signin-button"
+        disabled={emailError || passwordError}
+        onClick={handleSignin}
+      >
+        로그인
+      </Button>
+    </AuthForm>
   );
 };
 
